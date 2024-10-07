da81c68e962ce4e71acb8ab714f0cc48b001e4a4
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-10-07 20:51:35 +0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
-import './App.css'
+import './App.css';
+import Layout from './components/Layout';
+import About from './components/pages/About';
+import Home from './components/pages/Home';
+import Vans from './components/pages/Vans';
 
 function App() {
-
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+        </Route>
+      </>
+    )
+  );
 
   return (
     <>
-      <h1>This is my website</h1>
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
