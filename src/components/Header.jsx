1d2c9f0d48e8467a186cabe4872c841c96039c16
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-10-07 20:52:16 +0300

    initial commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className="header-container">
+      <Link to='/' className='logo'>pulsy</Link>
+      <div className="header-links">
+        <NavLink to='about'>About</NavLink>
+        <NavLink  to='vans'>Vans</NavLink>
+      </div>
+    </div>
   )
 }
