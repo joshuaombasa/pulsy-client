416e0a5ab265ed7b41e9fc3edd29194afd18e536
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-10-14 23:32:33 +0300

    initial commit

--- src/components/Hostlayout.jsx
+++ src/components/Hostlayout.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
 
 export default function Hostlayout() {
   return (
-    <div>Hostlayout</div>
+    <>
+     <div className="host-layout">
+      <NavLink to='.'>Dashboard</NavLink>
+      <NavLink to='income'>Income</NavLink>
+      <NavLink to='vans'>Vans</NavLink>
+      <NavLink to='reviews'>Reviews</NavLink>
+     </div>
+     <Outlet/>
+    </>
   )
 }
