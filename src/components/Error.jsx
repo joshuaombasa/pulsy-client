a8cf908a417b9376dc5c8ed896eeb40975f957f9
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-10-21 23:49:22 +0300

    initial commit

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Error() {
   return (
-    <div>Error</div>
-  )
+    <>
+      <div>Error</div>
+      <NavLink to="/">Back to Home</NavLink>
+    </>
+  );
 }
