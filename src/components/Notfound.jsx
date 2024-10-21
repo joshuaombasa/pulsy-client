c8633bdff914db717e9b347aeb57ab033920a5dd
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-10-21 23:49:08 +0300

    initial commit

--- src/components/Notfound.jsx
+++ src/components/Notfound.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Notfound() {
   return (
-    <div>Notfound</div>
-  )
+    <>
+      <div>Notfound</div>
+      <NavLink to="/">Back to Home</NavLink>
+    </>
+  );
 }
